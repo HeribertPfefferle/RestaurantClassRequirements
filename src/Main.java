14d8a524b165433f5c2fa84008426a6d23ac1344
HeribertPfefferle <Heribert.Pfefferle@arapahoe.edu>
2026-09-02 11:15:05 -0600

    Update

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    //public static void main(String[] args) {
+
+    System.out.println("Name: Edelweiss");
+    System.out.println("Wait Time: 30 min");
+    System.out.println("Rating: 4 (out of 5)");
+    System.out.println("Price: $25 - $35");
+    System.out.println("Hours Open:");
+    System.out.println("MO 10am - 11pm");
+    System.out.println("TU 10am - 11pm");
+    System.out.println("WE 10am - 11pm");
+    System.out.println("TH 10am - 11pm");
+    System.out.println("FR 10am - 11pm");
+    System.out.println("SA 2pam - 12pm");
+    System.out.println("SU 2pam - 12pm");
+}
+
